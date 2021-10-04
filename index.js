6c8b47bd0b3408450d8f0a35647cd5c66d122284
joanex01 <70920904+joanex01@users.noreply.github.com>
2021-10-04 13:49:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -44,8 +44,34 @@ const dialogflowFullfillment =(request, response) => {
                 //agent.add('Email enviado! Leia as informações adicionais: '+ info);
         });
     }
+    function envio_emailBackup(agent){
+        var nodemailer = require('nodemailer');
+        var sendmail = require('sendmail');
+        var transporter = nodemailer.createTransport({
+            sendmail: true,
+            newline: 'unix',
+            path: '/usr/sbin/sendmail',
+        });
+
+        var email = {
+            from:request.body.queryResult.parameters['email'], // Quem enviou este e-mail
+            to:"suporte@virtual.ufpb.br", // Quem receberá
+            subject: "Abertura de chamado pela Dorinha", // Um assunto
+            html:"Abertura de chamado com assunto: BACKUP DE TURMAS. Informações fornecidas pelo usuário: NOME COMPLETO DA DISCIPLINA:"+ request.body.queryResult.parameters['nomedisciplina']+", Período em que foi ministrada:"+ request.body.queryResult.parameters['periodo']+"."
+        };
+        transporter.sendMail(email,function(error, info){ 
+                //console.log(info.envelope);
+                //console.log(info.messageId);
+                if(error){
+                    console.log (error);
+                    throw error; // algo de errado aconteceu.
+                }
+                //agent.add('Email enviado! Leia as informações adicionais: '+ info);
+        });
+    }
     
     let intentMap = new Map();
-    intentMap.set("enviar email", envio_email)
+    intentMap.set("mail-ticketPadrao", envio_email)
+    intentMap.set("mail-backupTurmas", envio_emailBackup)
     agent.handleRequest(intentMap)
 }
